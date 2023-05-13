b551085eb13df16e259ca3ba62ad76c3f2127b20
jgressier <jeremie.gressier@isae.fr>
2023-05-13 11:01:03 +0200

    new DataSet tests for meshbase._data

--- cfdtools/meshbase/_data.py
+++ cfdtools/meshbase/_data.py
@@ -1,21 +1,17 @@
 import cfdtools.api as api
-
-
-
-# class indexeddata:
-#     def __init__(self, type='nodal', index='direct'):
-#         self._type = type
-#         self._index = index
+import numpy as np
 
 _available_Xrep = ('nodal', 'cellaverage', 'spectralcell')
 _available_Trep = ('instant', 'timeevol', 'fourier', 'pod')
-
 
 class DataSet():
 
     def __init__(self, Xrep='cellaverage', ndof=1, Trep='instant'):
         self._ndof = ndof
         self._data = dict()
+        self.Trep = Trep
+        self.Xrep = Xrep
+        self._properties = dict()
 
     @property
     def ndof(self):
@@ -32,6 +28,7 @@ class DataSet():
 
     @Xrep.setter
     def Xrep(self, Xrep):
+        assert Xrep in _available_Xrep
         self._Xrep = Xrep
 
     @property
@@ -40,17 +37,43 @@ class DataSet():
 
     @Trep.setter
     def Trep(self, Trep):
+        assert Trep in ('instant')
         self._Trep = Trep
 
     def add_data(self, name, data, time=None):
+        if time:
+            self._properties['time'] = time
         self._data[name] = data
 
     def data(self):
         return self._data
     
+    def keys(self):
+        return self._data.keys()
+
     def items(self):
         return self._data.items()
 
     def __getitem__(self, name):
         return self._data[name]
+
+# class manufactured_DataSet(DataSet):
+
+#     def __init__(self, datafunctions, Xrep='cellaverage', ndof=1, Trep='instant'):
+#         super().__init__(Xrep, ndof, Trep)
+
+class DataSetList(DataSet):
+
+    def __init__(self, ndataset, Xrep='cellaverage', ndof=1, Trep='timeevol'):
+        super().__init__(Xrep, ndof, Trep)
+        self._ndataset = ndataset
+
+    @property
+    def Trep(self):
+        return self._Trep
+
+    @Trep.setter
+    def Trep(self, Trep):
+        assert Trep in _available_Trep
+        self._Trep = Trep
 
