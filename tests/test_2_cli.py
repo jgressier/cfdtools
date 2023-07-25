6d5f1a8a0952774637cdf0d80647522b7d395e75
jgressier <jeremie.gressier@isae.fr>
2023-07-25 15:19:50 +0200

    test vtkbrief

--- tests/test_2_cli.py
+++ tests/test_2_cli.py
@@ -2,10 +2,9 @@ import cfdtools._cli as cli
 from pathlib import Path
 import pytest
 
-_datadir = Path("./tests/data")
-_builddir = Path("./tests/build")
 
 def outdirlist(nameaslist):
+    _builddir = "./tests/build/"
     return nameaslist + ["--outpath", str(_builddir)]
 
 
@@ -20,9 +19,9 @@ def test_info(filename):
     assert cli.info([filename])
 
 
-def test_cube_ic3():
+def test_cube_ic3(builddir):
     filename = "cube.ic3"
-    _builddir.mkdir(exist_ok=True)
+    builddir.mkdir(exist_ok=True)
     file1 = cli.writecube(outdirlist([filename]))
     assert file1
     Path(file1).unlink()
@@ -46,8 +45,8 @@ def test_ic3brief(filename):
         "./tests/data/Box3x3x2v2.ic3",
     ],
 )
-def test_ic3writev2(filename):
-    _builddir.mkdir(exist_ok=True)
+def test_ic3writev2(builddir, filename):
+    builddir.mkdir(exist_ok=True)
     file1 = cli.write_ic3v2(outdirlist([filename]))
     file2 = cli.write_ic3v2(outdirlist([filename]))  # write twice to test safe new name
     assert file1 != filename
@@ -64,30 +63,37 @@ def test_ic3writev2(filename):
         "./tests/data/nrg-o1hllc-vf-v3.ic3",
     ],
 )
-def test_ic3writev3(filename):
-    _builddir.mkdir(exist_ok=True)
+def test_ic3writev3(builddir, filename):
+    builddir.mkdir(exist_ok=True)
     file1 = cli.write_ic3v3(outdirlist([filename]))
     assert file1 != filename
     Path(file1).unlink()
 
 
+def test_vtkbrief(datadir: Path):
+    file = "cubemixed0000.vtu"
+    assert cli.vtkbrief([str(datadir / file)])
+
+
 @pytest.mark.parametrize("args", [["--fmt", "CGNS", "./tests/data/cavity-degen.hdf"]])
-def test_vtkwrite(args):
-    _builddir.mkdir(exist_ok=True)
+def test_vtkwrite(builddir, args):
+    builddir.mkdir(exist_ok=True)
     file1 = cli.write_vtk(outdirlist(args))
     assert file1
     Path(file1).unlink()
 
-def test_vtkwrite_extrude():
+
+def test_vtkwrite_extrude(builddir):
     args = ["--fmt", "CGNS", "--extrude", "5", "./tests/data/cavity-degen.hdf"]
-    _builddir.mkdir(exist_ok=True)
+    builddir.mkdir(exist_ok=True)
     file1 = cli.write_vtk(outdirlist(args))
     assert file1
     Path(file1).unlink()
 
-def test_vtkwrite_scale():
+
+def test_vtkwrite_scale(builddir):
     args = ["--fmt", "CGNS", "--scale", "2.", ".5", "1.", "./tests/data/cavity-degen.hdf"]
-    _builddir.mkdir(exist_ok=True)
+    builddir.mkdir(exist_ok=True)
     file1 = cli.write_vtk(outdirlist(args))
     assert file1
     Path(file1).unlink()
