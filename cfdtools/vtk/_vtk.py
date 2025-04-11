b8b68bb94de8a25f970916e451ff3c07f27fac0c
Gilles Grondin <gilles.grondin@isae.fr>
2025-04-11 17:05:46 +0200

    vtk(brief|diag): directly return if no pyvista

--- cfdtools/vtk/_vtk.py
+++ cfdtools/vtk/_vtk.py
@@ -112,18 +112,23 @@ class vtkMesh:
     def brief(self):
         if self._grid:
             m = self._grid
-            log.info(f"pyvista object: {m}")
-            log.info("> mesh")
-            log.info(f"  bounds : {m.bounds}")
-            log.info(f"  ncells : {m.n_cells}")
-            log.info(f"  npoints: {m.n_points}")
-            log.info("> data")
-            log.info(f"  cell  data names: {m.cell_data.keys()}")
-            log.info(f"  point data names: {m.point_data.keys()}")
-            log.info(f"  field data names: {m.field_data.keys()}")
-            # log.info("> properties")
         else:
             log.warning("  no pyvista mesh available")
+            return
+
+        mm = f"{m}"
+        log.info(f"pyvista object: {m}")
+        log.info(f"pyvista object:")
+        [log.info(f"|   {l}") for l in f"{m}".split(chr(10))]
+        log.info(f"> mesh")
+        log.info(f"   |  mesh bounds : {m.bounds}")
+        log.info(f"   |  mesh ncells : {m.n_cells}")
+        log.info(f"   |  mesh npoints: {m.n_points}")
+        log.info(f"> data")
+        log.info(f"   |  cell  data names: {m.cell_data.keys()}")
+        log.info(f"   |  point data names: {m.point_data.keys()}")
+        log.info(f"   |  field data names: {m.field_data.keys()}")
+        # log.info("> properties")
 
     def diag(self):
         if self._grid:
@@ -131,33 +136,134 @@ class vtkMesh:
         else:
             log.warning("  no pyvista mesh available")
             return
+
+        log.info(f"pyvista object:")
         #self.brief()
         volume = self.volumes()
         volmin, volmax = min(volume), max(volume)
         voltot, volava = sum(volume), np.mean(volume)
         vol_nb, volavg = len(volume), np.exp(np.mean(np.log(volume)))
-        if True:
-            log.info("> volumes")
-            log.info(f"  vol nb / total vol : {vol_nb:12d} / {voltot:12.6e}")
-            log.info(f"  (arith / geom) avg : {volava:12.6e} / {volavg:12.6e}")
-            log.info(f"  min vol / max vol  : {volmin:12.6e} / {volmax:12.6e}")
-            log.info(f"  max ratio b/nghbs  : {'???'}")
-            #k = 'connectivity'
-            #log.info(f"  dir(m.{k}): {(m.__getattr__(k))}")
-            #k = 'cell_connectivity'
-            #log.info(f"  dir(m.{k}): {(m.__getattr__(k)).shape}")
-            #for i in range(4):
-            #    print(m.cell_connectivity[16*i:16*(i+1)])
-            #print("...")
-            #for i in range(-4, 0):
-            #    print(m.cell_connectivity[16*i:min(16*(i+1),-1)])
-            #lk = []
-            #for k in dir(self):
-            #    if k[:2] == k[-2:]:
-            #        lk += [k]
-            #    else:
-            #        print(k)
-            #print(lk)
+
+        log.info(f"> volumes")
+        log.info(f"  vol nb / total vol :    {vol_nb:12d} / {voltot:12.6e}")
+        log.info(f"  (arith / geom) avg :    {volava:12.6e} / {volavg:12.6e}")
+        log.info(f"  min vol / max vol  :    {volmin:12.6e} / {volmax:12.6e}")
+
+        cell_points = []
+        point_cells = [[] for _ in range(m.n_points)]
+        idxc = 0
+        ic = 0
+        while idxc < len(m.cells):
+            nbp = m.cells[idxc]
+            idxc += 1
+            plist = m.cells[idxc:idxc + nbp]
+            idxc += nbp
+            # cell_points[ic] = [list of points of cell ic]
+            cell_points += [ plist ]
+            for ip in plist:
+            # point_cells[ip] = [list of cells of point ip]
+                point_cells[ip] += [ic]
+            ic += 1
+        lrat_node, lrat_edge, lrat_face = [0.0, 0.0, 0.0]
+        c_done = []
+        nb_log_comp = 0
+        nb_node_count = 0
+        nb_edge_count = 0
+        nb_face_count = 0
+        pdbg = False
+        psum = False
+        if pdbg:
+            pnt_com_dict = {}
+            edg_com_dict = {}
+            fac_com_dict = {}
+            print("processing")
+        # loop on all cells: ca
+        for ca in range(len(cell_points)):
+            c_done += [ca]
+            ca_done = []
+            if pdbg:
+                print(f"cell {ca:2}:")
+            # points of ca not already processed (as points of a previous ca)
+            ca_points = cell_points[ca]
+            # loop on those points of ca: pi
+            for pi in ca_points:
+                if pdbg:
+                    print(f"    point {pi:2}:") #, end='')
+                # cells of pi not already processed (as a previous ca, or as cb for this ca)
+                pi_cells = [c for c in point_cells[pi] if c not in c_done + ca_done]
+                # loop on those cells of pi: cb
+                for cb in pi_cells:
+                    ca_done += [cb]
+                    if pdbg:
+                        print(f"        {cb:2}", end='')
+                    # Compute the absolute volume ratio log of ca/cb
+                    lrat = abs(np.log(volume[ca]/volume[cb]))
+                    nb_log_comp += 1
+                    # Common points of ca and cb
+                    pcom = tuple(sorted(p for p in cell_points[ca] if p in cell_points[cb]))
+                    if len(pcom) == 1: # Only one common point
+                        lrat_node = max(lrat_node, lrat)
+                        nb_node_count += 1
+                        if pdbg:
+                            print("   ", "pcom:", end='')
+                            for p in pcom:
+                                if p not in pnt_com_dict:
+                                    pnt_com_dict[p] = []
+                                pnt_com_dict[p] += [[ca, cb]]
+                    elif len(pcom) == 2: # Two common points for an edge
+                        lrat_edge = max(lrat_edge, lrat)
+                        nb_edge_count += 1
+                        if pdbg:
+                            print("   ", "ecom:", end='')
+                            for p in pcom:
+                                if p == pi: continue
+                                k = (pi, p)
+                                if k not in edg_com_dict:
+                                    edg_com_dict[k] = []
+                                edg_com_dict[k] += [[ca, cb]]
+                    else: # len(pcom) > 2: more than two common points for a face
+                        lrat_face = max(lrat_face, lrat)
+                        nb_face_count += 1
+                        if pdbg:
+                            print("   ", "fcom:", end='')
+                            fac_com_dict |= {pcom: [[ca, cb]]}
+                    if pdbg:
+                        print("", pcom, [ca,cb])
+        if pdbg:
+            print("processing end")
+        if psum:
+            s_pnt_com_keys = sorted(pnt_com_dict.keys())
+            s_edg_com_keys = sorted(edg_com_dict.keys())
+            s_fac_com_keys = sorted(fac_com_dict.keys())
+            print("pnt_com_dict:")
+            pprev = None
+            for p in [_ for _ in s_pnt_com_keys]:
+                if p == pprev: continue
+                pprev = p
+                print("   ", {k: v for k, v in pnt_com_dict.items() if k == p})
+            print("edg_com_dict:")
+            eprev = None
+            for e in [_ for _ in s_edg_com_keys]:
+                if e == eprev: continue
+                eprev = e
+                print("   ", {k: v for k, v in edg_com_dict.items() if k == e})
+            print("fac_com_dict:")
+            fprev = None
+            for f in [_ for _ in s_fac_com_keys]:
+                if f == fprev: continue
+                fprev = f
+                print("   ", {k: v for k, v in fac_com_dict.items() if k == f})
+        log.info(f"computed ratios: {nb_log_comp:8d}")
+        log.info(f"    node ratios: {nb_node_count:8d}")
+        log.info(f"    edge ratios: {nb_edge_count:8d}")
+        log.info(f"    face ratios: {nb_face_count:8d}")
+        rat_node = np.exp(lrat_node)
+        rat_edge = np.exp(lrat_edge)
+        rat_face = np.exp(lrat_face)
+        log.info(f"  max vol ratio /nodes  : {rat_node:12.6e}")
+        log.info(f"  max vol ratio /edges  : {rat_edge:12.6e}")
+        log.info(f"  max vol ratio /faces  : {rat_face:12.6e}")
+
 
     def plot(self, background='white', show_edges=True, *args, **kwargs):
         self.pyvista_grid.plot(background=background, show_edges=show_edges, *args, **kwargs)
